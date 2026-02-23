5e11250aeb3169a4511089911781b77a8068e75c
Vilius S <asviliussimonavicius@gmail.com>
2026-02-23 17:49:55 +0200

    Confirmed cloudflare adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,9 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'; // Svarbu: būtent šitas kelias!
+import adapter from '@sveltejs/adapter-cloudflare';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Dokumentacija sako naudoti masyvą: [vitePreprocess()]
-	preprocess: [vitePreprocess()],
-
+	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter()
 	}
